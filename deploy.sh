ee7001d819115ce84879e1c767ce1e5ec03c5478
fikriamri <famri@alterra.id>
2019-08-28 14:06:28 +0700

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo docker stop fikriamri/e-commerce-frontend:v.2
-sudo docker rm fikriamri/e-commerce-frontend:v.2
+sudo docker stop frontend
+sudo docker rm frontend
 sudo docker rmi fikriamri/e-commerce-frontend:v.2
 sudo docker run -d -p 3000:80 --name frontend fikriamri/e-commerce-frontend:v.2
