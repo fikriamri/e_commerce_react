fe7e44489265da07bcc34b92108a19a58167aaa5
fikriamri <famri@alterra.id>
2019-08-28 12:41:08 +0700

    makasih mas

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdi
 docker stop fikriamri/e-commerce-frontend:v.2
 docker rm fikriamri/e-commerce-frontend:v.2
 docker rmi fikriamri/e-commerce-frontend:v.2
-docker run -d -p 3000:80 --name frontend fikriamri/e-commerce-frontend2:latest
+docker run -d -p 3000:80 --name frontend fikriamri/e-commerce-frontend2:v.2
