bf0d542cd582889e220cb1a7dc52c4646d26d77a
fikriamri <famri@alterra.id>
2019-08-28 14:05:53 +0700

    ungitignore build

---
+++ build/precache-manifest.99e95fc6b5e3acca782dbc01e5135309.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c092409e062ac764f0dfe4dcd83497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4b26527b1f7c9c049db",
+    "url": "/static/css/main.6eeeb13a.chunk.css"
+  },
+  {
+    "revision": "4471409babd5a72bb422",
+    "url": "/static/js/2.2a7a1c4b.chunk.js"
+  },
+  {
+    "revision": "a4b26527b1f7c9c049db",
+    "url": "/static/js/main.a1c17c32.chunk.js"
+  },
+  {
+    "revision": "9ce06c83e7fee4c5748a",
+    "url": "/static/js/runtime~main.4df2ecbd.js"
+  }
+]);
--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)" &&
-
-source ~/.profile
-echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
-docker stop fikriamri/e-commerce-frontend:v.2
-docker rm fikriamri/e-commerce-frontend:v.2
-docker rmi fikriamri/e-commerce-frontend:v.2
-docker run -d -p 3000:80 --name frontend fikriamri/e-commerce-frontend2:v.2
+sudo docker stop fikriamri/e-commerce-frontend:v.2
+sudo docker rm fikriamri/e-commerce-frontend:v.2
+sudo docker rmi fikriamri/e-commerce-frontend:v.2
+sudo docker run -d -p 3000:80 --name frontend fikriamri/e-commerce-frontend:v.2
